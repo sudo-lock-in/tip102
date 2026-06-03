9703c7e108eca14a2354b2a1b3c44f06957cb296
A T <amythompson0325@gmail.com>
2026-06-02 22:08:16 -0400

    I did the first few questions.

---
+++ week1session1.py
@@ -0,0 +1,25 @@
+def welcome():
+	print("Welcome to The Hundred Acre Wood!")
+ 
+welcome()
+
+def greeting(name):
+	print(f"Welcome to The Hundred Acre Wood {name}! My name is Christopher Robin.")
+ 
+greeting("Michael")
+greeting("Winnie the Pooh")
+
+def print_catchphrase(character):
+	match character:
+        case "Pooh":
+            print("Oh bother!")
+        case "Tigger":
+            print("Oh bother!")
+        case "Eeyore":
+            
+            
+             
+
+
+
+       
