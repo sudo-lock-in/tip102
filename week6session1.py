6ce73d375d0c867eaecfde34a0b3570574dcc28e
AJ <arthur.james.thompson6@gmail.com>
2026-06-20 15:30:32 -0400

    more practice. learned more floyd algorithms. need to fix partition problem. not finished w/ session 2

--- week6session1.py
+++ week6session1.py
@@ -75,35 +75,3 @@ def count_critical_points(song_audio):
 song_audio = Node(5, Node(3, Node(1, Node(2, Node(5, Node(1, Node(2)))))))
 
 # print(count_critical_points(song_audio))
-
-# session 2 problem
-class Node:
-    def __init__(self, value, next=None):
-        self.value = value
-        self.next = next
-
-def is_circular(clues):
-    curr = clues
-    while curr.next:
-        if curr.next == clues:
-            return True # put statement in here to avoid infinite loop because the curr.next will point to the head if true
-            # and continue endlessly
-            # so if any nodes point to head that is the looping/tail part
-            # if i did it like
-            # while curr.next:
-            #   curr = curr.next
-            # if curr.next == clues:
-            #   return True
-            # it wouldnt work because the while loop will keep going since there will always be a curr.next if it points to head
-            # so return inside the loop if it points to head to make it stop
-        curr = curr.next 
-    return False
-
-clue1 = Node("The stolen goods are at an abandoned warehouse")
-clue2 = Node("The mayor is accepting bribes")
-clue3 = Node("They dumped their disguise in the lake")
-clue1.next = clue2
-clue2.next = clue3
-clue3.next = clue1
-
-print(is_circular(clue1))
---
+++ week6session2.py
@@ -0,0 +1,97 @@
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+def is_circular(clues):
+    curr = clues
+    while curr.next:
+        if curr.next == clues:
+            return True # put statement in here to avoid infinite loop because the curr.next will point to the head if true
+            # and continue endlessly
+            # so if any nodes point to head that is the looping/tail part
+            # if i did it like
+            # while curr.next:
+            #   curr = curr.next
+            # if curr.next == clues:
+            #   return True
+            # it wouldnt work because the while loop will keep going since there will always be a curr.next if it points to head
+            # so return inside the loop if it points to head to make it stop
+        curr = curr.next 
+    return False
+
+clue1 = Node("The stolen goods are at an abandoned warehouse")
+clue2 = Node("The mayor is accepting bribes")
+clue3 = Node("They dumped their disguise in the lake")
+clue1.next = clue2
+clue2.next = clue3
+clue3.next = clue1
+
+# print(is_circular(clue1))
+
+
+def collect_false_evidence(evidence):
+    false = []
+    slow = evidence
+    fast = evidence # both start at evidence to keep the math in order. so we have to make them increment first before the condition in while
+    while fast and fast.next: # floyd cycle detection algorithm
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            break
+    else: # works with loops. if they break it will do this else block
+        return []
+    
+    # floyd algorithm to get to start of cycle. so we can capture every value within it
+    slow = evidence
+    while slow != fast:
+        slow = slow.next
+        fast = fast.next 
+        # visualize the pointers meeting up to a center point
+    
+    while fast: # will run inifnitely (its a cycle) until we find the cycle and break
+        false.append(fast.value)
+        fast = fast.next # incrememnt before checking again since both are equivalent from after cycle start check
+        if fast == slow: # slow stays at the beginning of our cycle for comparison
+            break
+    return false
+    
+
+clue1 = Node("Unmarked sedan seen near the crime scene")
+clue2 = Node("The stolen goods are at an abandoned warehouse")
+clue3 = Node("The mayor is accepting bribes")
+clue4 = Node("They dumped their disguise in the lake")
+clue1.next = clue2
+clue2.next = clue3
+clue3.next = clue4
+clue4.next = clue2
+
+clue5 = Node("A masked figure was seen fleeing the scene")
+clue6 = Node("Footprints lead to the nearby woods")
+clue7 = Node("A broken window was found at the back")
+clue5.next = clue6
+clue6.next = clue7
+
+# print(collect_false_evidence(clue1))
+# print(collect_false_evidence(clue5))
+
+def print_linked_list(head):
+    current = head
+    while current:
+        print(current.value, end=" -> " if current.next else "\n")
+        current = current.next
+
+def partition(suspect_ratings, threshold):
+    temp_head = Node("temp") 
+    temp_head.next = suspect_ratings 
+    curr = suspect_ratings
+    while curr.next:
+        if curr.value >= threshold and curr.value > curr.next.value:
+            curr.next = suspect_ratings
+        curr = curr.next
+    return suspect_ratings
+
+suspect_ratings = Node(1, Node(4, Node(3, Node(2, Node(5, Node(2))))))
+
+print_linked_list(partition(suspect_ratings, 3))
+
