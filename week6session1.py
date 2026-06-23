79754743678d756ce0b45d3d7df4c12d79f318fd
AJ <arthur.james.thompson6@gmail.com>
2026-06-22 22:56:58 -0400

    did some of problem set 2 for unit 6 session 1

--- week6session1.py
+++ week6session1.py
@@ -75,3 +75,93 @@ def count_critical_points(song_audio):
 song_audio = Node(5, Node(3, Node(1, Node(2, Node(5, Node(1, Node(2)))))))
 
 # print(count_critical_points(song_audio))
+
+
+
+
+# problem set 2
+
+head = Node("Jimin", Node("Taehyung", Node("Jungkook"))) # (im not doing h*rry p*tter lol... but ig for the rest i oughta)
+
+# print_linked_list(head)
+
+class Node:
+    def __init__(self, house, score, next=None):
+        self.house = house
+        self.value = score
+        self.next = next
+
+# For testing
+def print_linked_list(head):
+    current = head
+    while current:
+        print((current.house, current.value), end=" -> " if current.next else "\n")
+        current = current.next
+
+
+def count_element(house_points, score):
+    count = 0
+    curr = house_points
+    while curr:
+        if curr.value == score:
+            count += 1
+        curr = curr.next
+    return count
+
+house_points = Node("Gryffindor", 600, 
+                Node("Ravenclaw", 300,
+                    Node("Slytherin", 500,
+                        Node("Hufflepuff", 600))))                  
+
+# print(count_element(house_points, 600))
+
+
+
+class Node:
+    def __init__(self, potion, next=None):
+        self.potion = potion
+        self.next = next
+
+# For testing
+def print_linked_list(head):
+    current = head
+    while current:
+        print(current.potion, end=" -> " if current.next else "\n")
+        current = current.next
+
+def find_middle_potion(potions):
+    # curr = potions 
+    # length = 0
+
+    # while curr:
+    #     length += 1
+    #     curr = curr.next
+
+    # slow = potions
+    # fast = potions
+
+    # while fast and fast.next:
+    #     slow = slow.next
+    #     fast = fast.next.next
+
+    # if length % 2 == 1:
+    #     return slow.potion
+    # else:
+    #     return slow.next.potion
+
+    # more simple
+    slow = potions
+    fast = potions
+
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+
+    return slow.potion
+    
+potions1 = Node("Poison Antidote", Node("Shrinking Solution", Node("Trollblood Tincture")))
+potions2 = Node("Elixir of Life", Node("Sleeping Draught", Node("Babbling Beverage", Node("Aging Potion"))))
+
+# print(find_middle_potion(potions1))
+# print(find_middle_potion(potions2))
+
