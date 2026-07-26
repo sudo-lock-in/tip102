ea844304d45d6995919e9263f7c36c70fdd1c428
AJ <arthur.james.thompson6@gmail.com>
2026-07-26 15:12:38 -0400

    dynamic programming problems

--- week12session1.py
+++ week12session1.py
@@ -1,5 +1,7 @@
 def energy_on_nth_day(n):
     # similar to fibonacci
+    if n == 0:
+        return 0
     if n == 1 or n == 2:
         return 1
     dp = [0] * (n + 1)
@@ -14,4 +16,21 @@ def energy_on_nth_day(n):
 # print(energy_on_nth_day(2))
 # print(energy_on_nth_day(5))
 # print(energy_on_nth_day(7))
+
+def toph_training(cost):
+    n = len(cost)
+    dp = [0] * n
+    dp[0], dp[1] = cost[0], cost[1]
+    for i in range(2, n):
+        dp[i] = min(dp[i - 1], dp[i - 2]) + cost[i]
+    return min(dp[n - 1], dp[n - 2])
+    
+
+# Example Usage:
+
+# print(toph_training([10, 15, 20]))
+# print(toph_training([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]))
+
+def aang_wins(n):
+    pass
 
